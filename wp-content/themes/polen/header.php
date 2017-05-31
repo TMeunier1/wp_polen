de6702be5e4fb6690000a6ef5b008ff9c70dd935
Tibau <xiliar1888@hotmail.fr>
2017-05-31 09:47:30 +0200

    add link css

--- wp-content/themes/polen/header.php
+++ wp-content/themes/polen/header.php
@@ -8,6 +8,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 	<?php wp_head(); ?>
 </head>
 
