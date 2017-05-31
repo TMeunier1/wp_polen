8773460c6098d7520a26cad7be82a5639edd39ed
Tibau <xiliar1888@hotmail.fr>
2017-05-31 14:38:06 +0200

    Add jquery

--- wp-content/themes/polen/functions.php
+++ wp-content/themes/polen/functions.php
@@ -1,1 +1,10 @@
-<?php add_theme_support( 'post-thumbnails' ); ?>
+<?php
+    add_theme_support( 'post-thumbnails' );
+    wp_register_script( 'script', get_template_directory_uri() . '/script.js' );
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'script' );
+
+
+
+
+?>
