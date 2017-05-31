d50aff01aca513ed08c6fe4d9de446403d9a4dfe
Tibau <xiliar1888@hotmail.fr>
2017-05-31 09:29:02 +0200

    Theme init

---
+++ wp-content/themes/polen/header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage polen
+ * @since Polen 0.000a
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<?php wp_head(); ?>
+</head>
+
+<body>
